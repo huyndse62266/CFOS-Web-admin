7134e5fbd1175028e97ea7a22cc88493c7ac2b3d
huyndse62266 <soundlound@gmail.com>
2019-06-07 13:18:14 +0700

    add modal, upload image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,8 +15,12 @@ class Header extends Component {
                                         <span className="notification">5</span>
                                         <p className="d-lg-none d-md-block">
                                             Some Actions
-                                            </p>
+                                        </p>
                                     </a>
+                                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
+                                        <a className="dropdown-item" href="#">And another line of sample text here</a>
+                                        <a className="dropdown-item" href="#">And another line of sample text here</a>
+                                    </div>
                                 </li>
                                 <li className="nav-item dropdown">
                                     <a className="nav-link" id="navbarDropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -26,8 +30,10 @@ class Header extends Component {
                                             </p>
                                     </a>
                                     <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                                        <a className="dropdown-item" href="#">Profile</a>
-                                        <a className="dropdown-item" href="#">Settings</a>
+                                        <a className="dropdown-item" href="#" data-toggle="modal" data-target="#profileModal">
+                                            Profile
+                                        </a>
+                                        <a className="dropdown-item" href="#" data-toggle="modal" data-target="#changePasswordModal">Change Password</a>
                                         <div className="dropdown-divider" />
                                         <a className="dropdown-item" href="#">Log out</a>
                                     </div>
@@ -36,6 +42,55 @@ class Header extends Component {
                         </div>
                     </div>
                 </nav>
+                <div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Profile</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" id="changePasswordModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Change Password</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="OldPassword">Old Password</label>
+                                <input type="password" class="form-control" id="OldPassword" />
+                            </div>
+                            <div class="form-group">
+                                <label for="NewPassword">New Password</label>
+                                <input type="password" class="form-control" id="NewPassword"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="ConfirmNewPassword">Confirm New Password</label>
+                                <input type="password" class="form-control" id="ConfirmNewPassword" />
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/pages/Product/ProductActionPage/ProductActionPage.js
+++ src/pages/Product/ProductActionPage/ProductActionPage.js
@@ -88,11 +88,20 @@ export default class ProductActionPage extends Component {
                       </div>          
                     </div>             
                     <div className="row">
-                      <div className="col-md-12">
-                          <br/>
-                            <img src="" width={"70%"} height={"70%"} style={{margin: '0 auto'}}/>
-                            <input type="file" />
-                      </div>
+                    <div class="fileinput fileinput-new text-center" data-provides="fileinput">
+    <div class="fileinput-new thumbnail img-raised">
+        <img src="http://style.anu.edu.au/_anu/4/images/placeholders/person_8x10.png" alt="..."/>>
+    </div>
+    <div class="fileinput-preview fileinput-exists thumbnail img-raised"></div>
+    <div>
+        <span class="btn btn-raised btn-round btn-default btn-file">
+            <span class="fileinput-new">Select image</span>
+            <span class="fileinput-exists">Change</span>
+            <input type="file" name="..." />
+        </span>
+        <a href="#pablo" class="btn btn-danger btn-round fileinput-exists" data-dismiss="fileinput"><i class="fa fa-times"></i> Remove</a>
+    </div>
+</div>
                     </div>
                     <br/>             
                     <button type="submit" className="btn btn-primary pull-right">Create</button>
